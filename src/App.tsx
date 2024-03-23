57f74c5a42b8ec73ac713f04c606161aa9217ae7
henrytrust1111 <henrytrust1111@gmail.com>
2024-03-23 19:58:08 +0100

    commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 const App:React.FC = ()=>{
   return(
     <div className="App">
-      <span className="heading"></span>
+      <span className="heading">My Todos</span>
     </div>
   )
 }
