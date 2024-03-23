06c9e2bb3578273cdd84146255ffb0be9a971934
henrytrust1111 <henrytrust1111@gmail.com>
2024-03-23 22:24:10 +0100

    commit 6

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,12 +1,35 @@
+import { useRef } from "react";
+import "./styles.css";
 
-
-const InputField = () => {
-  return (
-    <form className="input">
-        {/* BEM (Block Element Modifier) */}
-        <input type="input" placeholder="Enter a task" className="input__box" />
-    </form>
-  )
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent)=> void;
 }
 
-export default InputField
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+  return ( 
+    <form className="input" onSubmit={(e)=>{
+        handleAdd(e)
+        inputRef.current?.blur()
+    }}>
+      {/* BEM (Block Element Modifier) */}
+      <input
+        type="input"
+        ref={inputRef}
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
