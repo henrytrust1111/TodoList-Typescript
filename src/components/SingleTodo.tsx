8c8fd14f7ed4199db84b41925087e8514bf3224d
henrytrust1111 <henrytrust1111@gmail.com>
2024-03-23 23:32:32 +0100

    commit 9

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,35 +1,48 @@
 import { AiFillDelete, AiFillEdit } from "react-icons/ai";
-import { Todo } from "../model"
+import { Todo } from "../model";
 import { MdDone } from "react-icons/md";
-import "./styles.css"
-
+import "./styles.css";
 
 type Props = {
-    todo: Todo;
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
 
-const SingleTodo:React.FC<Props> = ({todo, todos, setTodos}) => {
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <form className="todos__single">
-        <span className="todos__single--text">
-            {todo.todo}
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+
+      <div className="">
+        <span className="icon">
+          <AiFillEdit />
         </span>
-
-        <div className="">
-            <span className="icon">
-                <AiFillEdit />
-            </span>
-            <span className="icon">
-                <AiFillDelete />
-            </span>
-            <span className="icon">
-                <MdDone /> 
-            </span>
-        </div>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default SingleTodo
+export default SingleTodo;
