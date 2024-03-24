d6ccdc4a179392158b72cc71e311725b9c14518f
henrytrust1111 <henrytrust1111@gmail.com>
2024-03-24 15:36:28 +0100

    commit 12

--- src/model.ts
+++ src/model.ts
@@ -4,32 +4,32 @@ export interface Todo {
   isDone: boolean;
 }
 
-type Action =
-  | { type: "add"; payload: string }
-  | { type: "remove"; payload: number }
-  | { type: "done"; payload: number };
+// type Action =
+//   | { type: "add"; payload: string }
+//   | { type: "remove"; payload: number }
+//   | { type: "done"; payload: number };
 
-const TodoReducer = (state: Todo[], action: Action) => {
-  switch (action.type) {
-    case "add":
-      return [
-        ...state,
-        { id: Date.now(), todo: action.payload, isDone: false },
-      ];
-    case "remove":
-      return state.filter((todo) => todo.id !== action.payload);
-    case "done":
-      return state.map((todo) =>
-        todo.id !== action.payload ? { ...todo, isDone: !todo.isDone } : todo
-      );
-      default: 
-      return state
-  }
-};
+// const TodoReducer = (state: Todo[], action: Action) => {
+//   switch (action.type) {
+//     case "add":
+//       return [
+//         ...state,
+//         { id: Date.now(), todo: action.payload, isDone: false },
+//       ];
+//     case "remove":
+//       return state.filter((todo) => todo.id !== action.payload);
+//     case "done":
+//       return state.map((todo) =>
+//         todo.id !== action.payload ? { ...todo, isDone: !todo.isDone } : todo
+//       );
+//       default: 
+//       return state
+//   }
+// };
 
-import { useReducer } from "react";
-const ReducerExample = () => {
-  const [state, dispatch] = useReducer(TodoReducer, []);
+// import { useReducer } from "react";
+// const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
 
-  return <></>;
-};
+//   return <></>;
+// };
