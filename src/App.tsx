23062bc34d2388ba61bf53bbe4de9edbc6327976
henrytrust1111 <henrytrust1111@gmail.com>
2024-03-25 11:16:21 +0100

    popUpStyle

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import InputField from "./components/InputField";
 import { Todo } from "./model";
 import TodoList from "./components/TodoList";
 import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import PopUp from "./components/PopUp";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [ok, setOk] = useState<boolean>(true);
   const [todos, setTodos] = useState<Todo[]>([]);
   const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 
@@ -21,43 +23,52 @@ const App: React.FC = () => {
 
   // console.log(todos);
 
-  const onDragEnd = (result:DropResult)=>{
+  const onDragEnd = (result: DropResult) => {
     // console.log(result);
-    const {source,destination} = result
-    if(!destination) return
-    if(destination.droppableId===source.droppableId && destination.index===source.index) return
-    let add,active=todos,complete=completedTodos;
-    if(source.droppableId==="TodosList1"){
-      add = active[source.index]
-      active.splice(source.index, 1)
-    }else{
-      add = complete[source.index]
-      complete.splice(source.index, 1)
+    const { source, destination } = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+    let add,
+      active = todos,
+      complete = completedTodos;
+    if (source.droppableId === "TodosList1") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
     }
-    if(destination.droppableId==="TodosList1"){
-      active.splice(destination.index, 0, add)
-    }else{
-      complete.splice(destination.index, 0, add)
+    if (destination.droppableId === "TodosList1") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
     }
-    setCompletedTodos(complete)
-    setTodos(active)
-  }
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
 
   return (
-   <DragDropContext onDragEnd={onDragEnd}>
-     <div className="App">
-      <span className="heading">My Todos (created by HENRY)</span>
+    <>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="App">
+          {ok ? <PopUp setOk={setOk} /> : null}
+          <span className="heading">My Todos (created by HENRY)</span>
 
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+          <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
 
-      <TodoList
-        todos={todos}
-        setTodos={setTodos}
-        completedTodos={completedTodos}
-        setCompletedTodos={setCompletedTodos}
-      />
-    </div>
-   </DragDropContext>
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            completedTodos={completedTodos}
+            setCompletedTodos={setCompletedTodos}
+          />
+        </div>
+      </DragDropContext>
+    </>
   );
 };
 
--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 import { MdCancel } from 'react-icons/md'
 import "./PopStyle.css"
 
-const PopUp:React.FC = () => {
+
+interface Props{
+    setOk:React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const PopUp:React.FC<Props>  = ({setOk}) => {
   return (
     <div className="deletealert">
         <div className="deleteoption">
             <div className="undel">
-            <MdCancel className='undo'/>
-
+            {/* <MdCancel className='undo'/> */}
             </div>
             <p>Are you sure you want to delete this product?</p>
             <div className= "deletediv">
-                <button>DELETE</button>
+                <button onClick={()=>setOk(false)}>OK</button>
             </div>
         </div>
     </div>
